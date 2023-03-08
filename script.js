e5f9a94e45f580047e1341b263a2ef73b4dba2d3
João Vitor <joaovitor55670@gmail.com>
2023-03-08 20:08:58 -0300

    quase lá..

--- script.js
+++ script.js
@@ -2,31 +2,33 @@ let add = document.querySelector('#add');
 let items = document.querySelector('.items');
 let arr = [];
 
-function load(){
-    if(localStorage.getItem('task')){
-         arr = JSON.parse(localStorage.getItem('task'));
-            
-        
-            for(i=0; i < arr.length; i++){
-                let div = document.createElement('div')
-                div.setAttribute('class', 'card');
-                div.innerHTML += `${arr[i]}<ion-icon name="checkmark-circle-outline"></ion-icon>`
-                items.appendChild(div);
-                
+function load() {
+    if (localStorage.task) {
+        arr = JSON.parse(localStorage.getItem('task'));
+
+
+        for (i = 0; i < arr.length; i++) {
+            let div = document.createElement('div')
+            div.setAttribute('class', 'card');
+            div.innerHTML += `${arr[i]}<ion-icon name="checkmark-circle-outline"></ion-icon>`
+            items.appendChild(div);
+
 
         }
-        
+
         let ion = document.getElementsByName('checkmark-circle-outline');
         let card = document.querySelectorAll('.card');
-        card.forEach(function(c, i){
-            ion[i].addEventListener('click', function(){
+        card.forEach(function (c, i) {
+            ion[i].addEventListener('click', function () {
                 c.style = 'display:none';
                 arr = [];
-                
-            })
-        });
-    }
+                // localStorage.task = JSON.stringify(arr[i])
+                localStorage.task = JSON.stringify(arr[i]);
 
+
+            })
+        })
+    }
 }
 
 
@@ -40,92 +42,21 @@ add.addEventListener('click', function () {
     document.querySelector('#input').value = '';
     localStorage.setItem('task', JSON.stringify(arr));
     let ion = document.getElementsByName('checkmark-circle-outline');
-        let card = document.querySelectorAll('.card');
-        card.forEach(function(c, i){
-            ion[i].addEventListener('click', function(){
-                c.style = 'display:none';
-                arr = [];
-                
-            })
-        });
+    let card = document.querySelectorAll('.card');
+    card.forEach(function (c, i) {
+        ion[i].addEventListener('click', function () {
+            c.style = 'display:none';
+            arr = [];
+            localStorage.task = JSON.stringify(arr[i]);
+
+
+        })
+    });
 })
 
 
 
 
-let ion = document.getElementsByName('checkmark-circle-outline');
 
-// ion.forEach(function(e, i){
-//     console.log(e)
-// })
-// let card = document.querySelectorAll('.card');
-// card.forEach(function(c, i){
-//     ion[i].addEventListener('click', function(){
-//         c.style = 'display:none';
-//         arr = [];
-        
-//     })
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// card.forEach(function(c, i){
-//     console.log(c)
-//     ion[i].addEventListener('click', function(){
-//         c.innerHTML = ''
-//     })
-// })
-
-
-
-
-// let card = document.querySelectorAll('.card')
-
-
-// card.forEach(function (c, i) {
-    
-//     let ion = document.getElementsByName('checkmark-circle-outline');
-    
-    // c.innerHTML += '<ion-icon name="checkmark-circle-outline"></ion-icon>'
-    
-    
-//     let div = document.createElement('div');
-//     div.setAttribute('class', 'card')
-    
-//     div.innerHTML = '<ion-icon name="checkmark-circle-outline"></ion-icon>'
-
-//     c.appendChild(div)
-//     console.log(div)
-
-//     ion[i].addEventListener('click', function(){
-//         c.innerHTML = ''
-//     })
-
-// })
-
-
-
-    
-
-    
 
 
