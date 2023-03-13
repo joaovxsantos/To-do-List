f5b377b8753f7c5aad074a4e711534fe279ab209
João Vitor <joaovitor55670@gmail.com>
2023-03-13 17:08:27 -0300

    :feat: funcionalidade JS

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let arr = [];
 
 //Função a ser executada quando a página carregar.
 function load() {
+
     //Aqui testo se o atributo 'task' existe, que é o atributo em que coloquei as tarefas.
     if (localStorage.task) {
         arr = JSON.parse(localStorage.getItem('task'));
@@ -17,7 +18,7 @@ function load() {
         for (i = 0; i < arr.length; i++) {
             let div = document.createElement('div')
             div.setAttribute('class', 'card');
-            div.innerHTML += `${arr[i]}<ion-icon name="checkmark-circle-outline"></ion-icon>`
+            div.innerHTML += `<p>${arr[i]}</p><ion-icon name="checkmark-circle-outline"></ion-icon>`
             items.appendChild(div);
 
             //Porém se o array estiver vazio, nada será mostrado na tela ao carregar a página.
@@ -47,18 +48,39 @@ function load() {
 //Botão de adicionar tarefas.
 add.addEventListener('click', function () {
     //Pegando o valor de input, que são as tarefas digitadas pelo usuário e setando uma div para expor os itens.
-    let input = document.querySelector('#input').value;
-    let div = document.createElement('div')
-    div.setAttribute('class', 'card');
-    div.innerHTML = `${input}<ion-icon name="checkmark-circle-outline"></ion-icon>`
-    items.appendChild(div)
+    let input = document.querySelector('#input').value.toUpperCase();
+    let div = document.createElement('div');
 
-    //Passando a tarefa para a ultima posição do array.
-    arr.push(input);
 
+    if (!input) {
+        alert('Digite a tarefa no campo indicado!');
+        div.style = 'display:none';
+    }
+
+    let num = 0
+    arr.forEach(function (e, i) {
+
+        if (arr[i] == input && input.length >= 1) {
+            alert('A tarefa já se encontra na lista.');
+            div.style = 'display:none';
+            num += 1;
+            console.log(num)
+        }
+    })
+
+    if (num < 1) {
+        //Setando atributo class com o valor card para a div, passando seu inner e após isso adicionando como filho de items.
+        div.setAttribute('class', 'card');
+        div.innerHTML = `<p>${input}</p><ion-icon name="checkmark-circle-outline"></ion-icon>`
+        items.appendChild(div);
+
+        //Passando a tarefa para a ultima posição do array.
+        arr.push(input);
+    }
+
+    //Limpando o input;
     document.querySelector('#input').value = '';
     localStorage.setItem('task', JSON.stringify(arr));
-
 
     //Percorrendo o array card.
     let ion = document.getElementsByName('checkmark-circle-outline');
